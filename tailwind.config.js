309c369dda43d4c934967f1eb1bf14933bd50cd6
GoodnessObi <goodnessobi@gmail.com>
2024-03-16 18:04:54 +0100

    feat: import assets and display page content

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
 			},
 			backgroundImage: {
 				'footer-bg': "url('./images/footer.png')",
+				'faq-bg': "url('./images/bg_curved2.png')",
+				'bedroom-bg': "url('./images/bedroom_bg.png')",
+				'livingroom-bg': "url('./images/livingroom_bg.png')",
+				'stools-bg': "url('./images/blurred-blue-stools.png')",
+				'gym-bg': "url('./images/gym_bg.png')",
+				'main-bg': "url('./images/bg_curved1.png')",
+				'hero-bg': "url('./images/hero_bg.png')",
 			},
 		},
 	},
